4facad2758504b94b34f0d9b49f860cd77412e18
Ravi Dhanure <Ravi Dhanure@DESKTOP-2FC8JKB>
2021-12-21 14:02:14 +0530

    see all armstrong numbers

---
+++ src/ArmstrongNumbers/Armstrong.java
@@ -0,0 +1,30 @@
+package ArmstrongNumbers;
+
+public class Armstrong {
+	public static void main(String[] args) 
+	{
+
+	int n=0;
+
+	for(int a=100;a<=999;a++)
+	{
+	for (int i=a;i>0;i=i/10)
+	{
+		int rem=i%10;
+		n=n+(rem*rem*rem);
+
+	}
+	if(n==a)
+
+	{
+		System.out.println(n+"  ");
+	}
+
+	n=0;
+	}
+
+	//System.out.println(n);
+		
+		
+	}
+	}
